f1daeefa7184a943d31f5ada78153e0acae92630
Dan Wyatt <git@danwyatt.co.uk>
2025-10-17 01:51:33 +0100

    Add flower graphic into footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,25 @@
+import Image from "next/image"
 import Link from "next/link"
+import BackgroundFlowers from "@/public/watercolour-flowers.png"
 
 export function Footer() {
   return (
     <footer className="bg-white/50 border-t border-wedding-text/10">
-      <div className="max-w-6xl mx-auto px-4 py-8">
-        <div className="flex flex-col md:flex-row items-center justify-between mb-6">
-          {/* Logo */}
-          <Link href="/" className="font-serif text-2xl text-wedding-text flex items-center gap-2 mb-4 md:mb-0">
+      <div className="max-w-6xl mx-auto px-4 pt-4 pb-8">
+        <div className="flex flex-col md:flex-row items-center justify-between mb-4">
+          <Image
+            src={BackgroundFlowers}
+            height={100}
+            alt=""
+            className="mb-4 md:mb-0"
+          />
+
+          <Link href="/" className="font-serif text-2xl text-wedding-text flex items-center gap-2 mb-4 md:mb-0 md:order-first">
             <span>Amy</span>
             <span className="font-script -mx-1 text-3xl/2 text-wedding-accent">and</span>
             <span>Daniel</span>
           </Link>
 
-          {/* Wedding Date */}
           <div className="text-wedding-text/80 text-center md:text-right">
             <p className="text-base font-medium">Save the Date</p>
             <p className="text-xl">Saturday, 2nd May 2026</p>
